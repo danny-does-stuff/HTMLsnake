445bfeced2ba73ae4cdc9236ba01a6f5afd3d4f0
Danny Harding <dannyharding10@gmail.com>
2017-01-24 17:14:44 -0700

    get jquery up in it

--- snake.js
+++ snake.js
@@ -1,1 +1,12 @@
-alert('test');
+
+/*include jQuery*/
+/*var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-3.1.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);*/
+
+$(document).ready(function() {
+	$('#play-button').click(function() {
+		alert('clicked');
+	});
+});
